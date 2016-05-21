fb9255031d2339834cb17c7740a3b63262abdfa9
James Gilliland <neclimdul@gmail.com>
2016-05-21 16:28:05 -0500

    Try merging repository

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm -t timhaak/plex .
+docker build --rm -t timhaak/plex-base Base
+docker build --rm -t timhaak/plex Plex
+docker build --rm -t timhaak/plex-pass PlexPass
--- buildNoCache.sh
+++ buildNoCache.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm --pull --no-cache -t timhaak/plex .
+docker build --rm --pull --no-cache -t timhaak/plex-base Base
+docker build --rm --pull --no-cache -t timhaak/plex Plex
+docker build --rm --pull --no-cache -t timhaak/plex-pass PlexPass
