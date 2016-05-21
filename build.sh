2fc874fbd83a01d77f8a211b1ca8f44235350b24
James Gilliland <neclimdul@gmail.com>
2016-05-21 17:01:39 -0500

    Use tags better

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm -t timhaak/plex-base Base
-docker build --rm -t timhaak/plex Plex
-docker build --rm -t timhaak/plex-pass PlexPass
+docker build --rm -t timhaak/plex:base Base
+docker build --rm -t timhaak/plex:plex Plex
+docker build --rm -t timhaak/plex:plex-pass PlexPass
--- buildNoCache.sh
+++ buildNoCache.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm --pull --no-cache -t timhaak/plex-base Base
-docker build --rm --pull --no-cache -t timhaak/plex Plex
-docker build --rm --pull --no-cache -t timhaak/plex-pass PlexPass
+docker build --rm --pull --no-cache -t timhaak/plex:base Base
+docker build --rm --pull --no-cache -t timhaak/plex:plex Plex
+docker build --rm --pull --no-cache -t timhaak/plex:plex-pass PlexPass
