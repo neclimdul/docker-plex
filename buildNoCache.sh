470b78157dcf2d939eb95f738882c94ec4b11f8c
James Gilliland <neclimdul@gmail.com>
2016-07-31 20:43:16 -0500

    Build my docker images

--- buildNoCache.sh
+++ buildNoCache.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm --pull --no-cache -t timhaak/plex:base Base
-docker build --rm --pull --no-cache -t timhaak/plex:plex Plex
-docker build --rm --pull --no-cache -t timhaak/plex:plex-pass PlexPass
+docker build --rm --pull --no-cache -t neclimdul/docker-plex:base Base
+docker build --rm --pull --no-cache -t neclimdul/docker-plex:plex Plex
+docker build --rm --pull --no-cache -t neclimdul/docker-plex:plex-pass PlexPass
