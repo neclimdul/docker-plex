9d5dbb7e92eb59103328505f7dd519d2fd402b67
James Gilliland <jgilliland@apqc.org>
2017-04-23 22:33:26 -0500

    Refactor into phusion/baseimage
    This make startup a bit easier by splitting the entrypoint from the complex setup script.
    
    Additionally there are some additional to parallelize the file permissions(when setting the data directory permissions) as well as some other cleanups.
    
    There was also some issues with starting up on the ubuntu images. Strangely the 16.04 image is saying its 16.10 so something might be a bit broken...
    
    Downside the images are bigger.

--- refresh.sh
+++ refresh.sh
@@ -8,7 +8,8 @@ sed -e "s#\${PLEX_URL}#$PLEX_PASS_URL#" Dockerfile.template > PlexPass/Dockerfil
 
 for i in Plex PlexPass; do
 	cp install.sh $i
-	cp start.sh $i
+	cp plex.sh $i
+	cp bootstrap.sh $i
 	cp Preferences.xml $i
 done
 #cd Plex && ln -sf ../install.sh && ln -sf ../start.sh && ln -sf ../Preferences.xml && cd -
