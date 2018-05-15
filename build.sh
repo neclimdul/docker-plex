680b309c6658d4b6a041850bb66b866825eb5338
James Gilliland <neclimdul@gmail.com>
2018-05-15 11:48:56 -0500

    Remove old base image build from process

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm -t neclimdul/docker-plex:base Base
+
 docker build --rm -t neclimdul/docker-plex:plex Plex
 docker build --rm -t neclimdul/docker-plex:plex-pass PlexPass
--- buildNoCache.sh
+++ buildNoCache.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build --rm --pull --no-cache -t neclimdul/docker-plex:base Base
+
 docker build --rm --pull --no-cache -t neclimdul/docker-plex:plex Plex
 docker build --rm --pull --no-cache -t neclimdul/docker-plex:plex-pass PlexPass
